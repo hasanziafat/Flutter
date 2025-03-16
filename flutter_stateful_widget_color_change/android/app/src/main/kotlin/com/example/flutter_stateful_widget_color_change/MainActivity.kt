9ee6c96b028ee93430753f210080023994b3708b
hasanziafat <ziafat.int@gmail.com>
2025-03-16 22:19:00 +0330

    Clear master branch content

--- flutter_stateful_widget_color_change/android/app/src/main/kotlin/com/example/flutter_stateful_widget_color_change/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_stateful_widget_color_change
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- simple_visit_card/android/app/src/main/kotlin/com/example/simple_visit_card/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.simple_visit_card
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
